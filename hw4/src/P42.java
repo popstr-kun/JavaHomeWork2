0124e6e34c66973550ba38a9e808b0fe998d646f
popstr-kun <hw38752920@gmail.com>
2022-10-21 14:58:25 +0800

    p42

---
+++ hw4/src/P42.java
@@ -0,0 +1,9 @@
+public class P42 {
+    public static void main(String[] args){
+        int[] test = {80,45,12,14,78};
+
+        for(int i=0; i<5; i++){
+            System.out.println("第"+(i+1)+"個人的分數是"+test[i]+"分");
+        }
+    }
+}
