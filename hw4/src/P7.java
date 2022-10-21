64340e736ed08eeb46e3f332f912e52c78940660
popstr-kun <hw38752920@gmail.com>
2022-10-21 13:32:32 +0800

    p7

---
+++ hw4/src/P7.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P7 {
+    public static void main(String[] args)throws IOException{
+        System.out.print("請輸入整數");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+
+        if(num == 1){
+            System.out.println("輸入的是1");
+            System.out.println("選擇的是1");
+        }
+
+        System.out.println("結束處理");
+    }
+}
